3710185f41a81c9bd970ce9c35fe7fc6f9d43e3a
levihernan <levi.hernan@gmail.com>
2016-12-15 16:13:00 -0300

    + dumb data .js

---
+++ js/dumb-data.js
@@ -0,0 +1,47 @@
+var jujuy_size = [
+  {label:"Pequeña", value:21.4},
+  {label:"MT1", value:6},
+  {label:"MT2", value:0.1},
+  {label:"Micro", value:72.5}
+];
+var jujuy_sector = [
+  {label:"Comercio", value:27.2},
+  {label:"Agropecuario", value:14.5},
+  {label:"Industria", value:16.9},
+  {label:"Mineria", value:0.2},
+  {label:"Servicios", value:38.2},
+  {label:"Construcción", value:3}
+];
+var jujuy_empleador = [
+  {label:"No empleadoras", value:37},
+  {label:"Empleadoras", value:63}
+];
+var jujuy_info = {
+  name: "Jujuy",
+  pymes: 78000,
+  pymes_por: 0.5
+};
+
+var cordoba_size = [
+  {label:"Pequeña", value:25.4},
+  {label:"MT1", value:5},
+  {label:"MT2", value:0.8},
+  {label:"Micro", value:52.5}
+];
+var cordoba_sector = [
+  {label:"Comercio", value:37.2},
+  {label:"Agropecuario", value:24.5},
+  {label:"Industria", value:6.9},
+  {label:"Mineria", value:2.2},
+  {label:"Servicios", value:33.2},
+  {label:"Construcción", value:13}
+];
+var cordoba_empleador = [
+  {label:"No empleadoras", value:47},
+  {label:"Empleadoras", value:63}
+];
+var cordoba_info = {
+  name: "Cordoba",
+  pymes: 45000,
+  pymes_por: 0.2
+}
