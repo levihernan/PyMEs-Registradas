47c1679bfdd2d546376c1594be27c1fe78ef05fe
levihernan <levi.hernan@gmail.com>
2016-12-16 16:22:54 -0300

    fixed changeDonuts

--- js/changeDonuts.js
+++ js/changeDonuts.js
@@ -1,8 +1,8 @@
 function changeDonuts(provincia) {
-  var svg = d3.select("#donut");
+  svg = d3.select("#donut");
   change(provincia.size);
-  var svg = d3.select("#donut2");
+  svg = d3.select("#donut2");
   change(provincia.sector);
-  var svg = d3.select("#donut3");
+  svg = d3.select("#donut3");
   change(provincia.empleador);
 }
