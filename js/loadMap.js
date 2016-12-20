a7887e863951a7dc525bd3a4f38808ec0ffb37d5
levihernan <levi.hernan@gmail.com>
2016-12-20 14:01:06 -0300

    fixes varios

--- js/loadMap.js
+++ js/loadMap.js
@@ -36,8 +36,8 @@ var color = d3.scale.quantize()
 
 
 
-d3.json("http://www.produccion.gob.ar/wp-content/uploads/2016/12/registropymes/js/argentina.geojson",function(error,geodata) {
-// d3.json("js/argentina.geojson",function(error,geodata) {
+// d3.json("http://www.produccion.gob.ar/wp-content/uploads/2016/12/registropymes/js/argentina.geojson",function(error,geodata) {
+d3.json("js/argentina.geojson",function(error,geodata) {
   if (error) return console.log(" error viejaaaa"); //unknown error, check the console
 
   //Create a path for each map feature in the data
